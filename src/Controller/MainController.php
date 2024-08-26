40255d81de6e61e3a957997238a1b012f24bfb10
Axysse <nicolas.vouillerot@gmail.com>
2024-08-26 16:26:43 +0200

    fait

---
+++ src/Controller/MainController.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Product;
+use App\Form\AjoutProduitType;
+use Doctrine\Migrations\Events;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
+
+class MainController extends AbstractController
+{
+    #[Route('/', name: 'app_main')]
+    public function index(EntityManagerInterface $entity): Response
+    {
+
+        $products = $entity->getRepository(Product::class)->findAll();
+        return $this->render('main/index.html.twig', [
+            'controller_name' => 'MainController',
+            'products' => $products
+        ]);
+    }
+
+    #[Route('/main/produit{id}', name: 'app_produit')]
+    public function produit($id, EntityManagerInterface $entity): Response
+    {
+        $products = $entity->getRepository(Product::class)->findAll();
+
+        $product = $entity->getRepository(Product::class)->findOneBy(['id' => $id]);
+
+        if (isset($_POST['supprimer'])) {
+            $del = $entity->getRepository(Product::class)->find($_POST['supprimer']);
+            $entity->remove($del);
+            $entity->flush();
+            return $this->redirectToRoute('app_main');
+        }
+
+        if (isset($_POST['modifier'])) {
+            $update = $entity->getRepository(Product::class)->find($_POST['modifier']);
+            
+                $update->setName($_POST['name']);
+                $update->setDescription($_POST['description']);
+                $update->setPrice($_POST['price']);
+                $update->setStock($_POST['stock']);
+                $update->setAuthor($_POST['author']);
+                $entity->flush();
+                return $this->redirectToRoute('app_main');
+            
+        }
+
+        return $this->render('main/produit.html.twig', [
+            'controller_name' => 'MainController',
+            'products' => $products,
+            'product' => $product
+        ]);
+    }
+
+    #[Route('/main/soumission', name: 'app_soumission')]
+    public function soumission(Request $request, EntityManagerInterface $em, SluggerInterface $slugger): Response
+    {
+
+        $product = new Product();
+
+        $form = $this->createForm(AjoutProduitType::class, $product);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $file = $form->get('image')->getData();
+
+            if ($file) {
+
+                $originalFilename = pathinfo($file->getClientOriginalName(), PATHINFO_FILENAME);
+                // this is needed to safely include the file name as part of the URL
+                $safeFilename = $slugger->slug($originalFilename);
+                $newFilename = 'images/' . $safeFilename . '-' . uniqid() . '.' . $file->guessExtension();
+
+
+                $file->move(
+                    $this->getParameter('kernel.project_dir') . '/assets/images',
+                    $newFilename
+                );
+                $product->setImage($newFilename);
+            }
+            $em->persist($product);
+            $em->flush();
+            $this->addFlash("success", "Ajout du produit réussit");
+            return $this->redirectToRoute('app_main');
+        }
+
+        // $products = $em->getRepository(Product::class)->findAll();
+        return $this->render('main/soumission.html.twig', [
+            'controller_name' => 'MainController',
+            'form' => $form->createView()
+        ]);
+    }
+}
---
+++ src/Repository/IdentifiantsRepository.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Identifiants;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\PasswordUpgraderInterface;
+
+/**
+ * @extends ServiceEntityRepository<Identifiants>
+ */
+class IdentifiantsRepository extends ServiceEntityRepository implements PasswordUpgraderInterface
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Identifiants::class);
+    }
+
+    /**
+     * Used to upgrade (rehash) the user's password automatically over time.
+     */
+    public function upgradePassword(PasswordAuthenticatedUserInterface $user, string $newHashedPassword): void
+    {
+        if (!$user instanceof Identifiants) {
+            throw new UnsupportedUserException(sprintf('Instances of "%s" are not supported.', $user::class));
+        }
+
+        $user->setPassword($newHashedPassword);
+        $this->getEntityManager()->persist($user);
+        $this->getEntityManager()->flush();
+    }
+
+    //    /**
+    //     * @return Identifiants[] Returns an array of Identifiants objects
+    //     */
+    //    public function findByExampleField($value): array
+    //    {
+    //        return $this->createQueryBuilder('i')
+    //            ->andWhere('i.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->orderBy('i.id', 'ASC')
+    //            ->setMaxResults(10)
+    //            ->getQuery()
+    //            ->getResult()
+    //        ;
+    //    }
+
+    //    public function findOneBySomeField($value): ?Identifiants
+    //    {
+    //        return $this->createQueryBuilder('i')
+    //            ->andWhere('i.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->getQuery()
+    //            ->getOneOrNullResult()
+    //        ;
+    //    }
+}
